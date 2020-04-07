487a295582e99723708575de49144ce265e84caf
João Almeida <joao@campuscode.com.br>
2020-04-06 21:16:47 -0300

    Create manufacturers

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -1,9 +1,31 @@
 class ManufacturersController < ApplicationController
   def index
     @manufacturers = Manufacturer.all
+    render 'index'
   end
 
   def show
     @manufacturer = Manufacturer.find(params[:id])
+    render 'show'
+  end
+
+  def new
+    @manufacturer = Manufacturer.new
+    render 'new'
+  end
+
+  def create
+    name = params[:manufacturer][:name]
+    @manufacturer = Manufacturer.new
+    @manufacturer.name = name
+
+    if @manufacturer.save
+      redirect_to @manufacturer
+    else
+      render 'new'
+    end
+  end
+
+  def update
   end
 end
---
+++ spec/features/admin_register_manufacturer_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+feature 'Admin register manufacturer' do
+  scenario 'successfully' do
+    # Arrange -> popular o BD
+  
+    # Act -> começa com um visit
+    visit root_path
+    click_on 'Fabricantes'
+    click_on 'Registrar novo fabricante'
+
+    fill_in 'Nome', with: 'Fiat'
+    click_on 'Enviar'
+
+    expect(current_path).to eq manufacturer_path(Manufacturer.last)
+    expect(page).to have_content('Fiat')
+  end
+
+  scenario 'and name can not be blank' do
+    visit root_path
+    click_on 'Fabricantes'
+    click_on 'Registrar novo fabricante'
+
+    fill_in 'Nome', with: ''
+    click_on 'Enviar'
+
+    expect(Manufacturer.count).to eq 0
+    expect(page).to have_content('Nome não pode ficar em branco')
+  end
+
+  scenario 'and name must be unique' do
+    Manufacturer.create!(name: 'Fiat' )
+    visit root_path
+    click_on 'Fabricantes'
+    click_on 'Registrar novo fabricante'
+
+    fill_in 'Nome', with: 'Fiat'
+    click_on 'Enviar'
+
+    expect(page).to have_content('Nome deve ser único')
+  end
+end
