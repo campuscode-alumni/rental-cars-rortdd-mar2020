6115333b85ef088a948d402eb533b7f37bcdbefb
João Almeida <joao@campuscode.com.br>
2020-04-08 20:46:00 -0300

    Editar fabricante

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -1,28 +1,47 @@
 class ManufacturersController < ApplicationController
+  before_action :set_manufacturer, only: [:show, :edit, :update]
+
   def index
     @manufacturers = Manufacturer.all
-    render 'index'
   end
 
   def show
-    @manufacturer = Manufacturer.find(params[:id])
-    render 'show'
   end
 
   def new
     @manufacturer = Manufacturer.new
-    render 'new'
   end
 
   def create
-    name = params[:manufacturer][:name]
-    @manufacturer = Manufacturer.new
-    @manufacturer.name = name
-
+    @manufacturer = Manufacturer.new(manufacturer_params)
     if @manufacturer.save # executa o método valid? 
       redirect_to @manufacturer
     else
       render 'new'
     end
   end
+
+  def edit
+  end
+
+  def update
+    if @manufacturer.update(manufacturer_params)
+      redirect_to @manufacturer
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def set_manufacturer
+    @manufacturer = Manufacturer.find(params[:id])
+  end
+
+  def manufacturer_params
+    # { name: params[:manufacturer][:name] }
+    params.require(:manufacturer).permit(:name, :cnpj)
+  end
+
+
 end
