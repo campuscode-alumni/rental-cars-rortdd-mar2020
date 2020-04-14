cc41020e2c42e8a1a436518c62ecfb7d4f885ee9
Joao Almeida <joaorsalmeida@gmail.com>
2020-04-14 20:36:08 -0300

    Admin view car models

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resources :manufacturers
   resources :car_categories, only: [:index, :new, :create, :show]
+  resources :car_models, only: [:index]
 end
---
+++ spec/features/admin_regiter_car_model_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+feature 'Admin register car model' do
+  scenario 'successfully' do
+    
+  end
+end
