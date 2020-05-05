486f2029dba6d9d70157a5c830867f0090c8d4a0
Joao Almeida <joaorsalmeida@gmail.com>
2020-05-05 20:09:37 -0300

    Nested resources: car_rental

--- app/controllers/car_rentals_controller.rb
+++ app/controllers/car_rentals_controller.rb
@@ -1,14 +1,15 @@
 class CarRentalsController < ApplicationController
   def create
     @car_rental = CarRental.new(car_rentals_params)
-    @rental = @car_rental.rental
+    @rental = Rental.find(params[:rental_id])
 
+    @car_rental.rental = @rental
     @car_rental.user = current_user
     @car_rental.start_date = Time.zone.now
+    @car_rental.daily_rate = @rental.car_category.daily_rate
 
     @rental.ongoing!
     @car_rental.car.rented!
-    @car_rental.daily_rate = @rental.car_category.daily_rate
     @car_rental.save!
 
     redirect_to @rental
@@ -17,7 +18,7 @@ class CarRentalsController < ApplicationController
   private
 
   def car_rentals_params
-    params.require(:car_rental).permit(:car_id, :rental_id, add_on_ids: [])
+    params.require(:car_rental).permit(:car_id, add_on_ids: [])
   end
 
   
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user = User.create!(email: 'test@test.com', password: '12345678')
+
+car_category = CarCategory.create!(name: 'A', daily_rate: 100, 
+                                    car_insurance: 100, third_part_insurance: 100)
+
+another_category = CarCategory.create!(name: 'B', daily_rate: 200, 
+                                       car_insurance: 200, third_part_insurance: 200)  
+
+fiat = Manufacturer.create!(name: 'Fiat')
+
+mobi = CarModel.create!(name: 'Mobi', manufacturer: fiat, car_category: car_category)
+argos = CarModel.create!(name: 'Argos', manufacturer: fiat, car_category: another_category)
+
+car = Car.create(car_model: mobi, license_plate: 'ABC-1234', mileage: 1000, color: 'Azul')
+another_car = Car.create(car_model: argos, license_plate: 'XYZ-9876', mileage: 0, color: 'Preto')
+
+customer = Customer.create!(name: 'Fulano Sicrano', cpf: '57810023594',
+                            email: 'teste@teste.com.br')
+
+addon = AddOn.create!(name: 'Carrinho de Bebê', daily_rate: 59, serial_number: 'BEBE1234')
+other_addon = AddOn.create!(name: 'Transporte para Bike', daily_rate: 29, serial_number: 'BIKE999')
+unused_addon = AddOn.create!(name: 'Carregador para Celular', daily_rate: 9, serial_number: 'CHARG567')
+
+rental = Rental.create!(start_date: '16/04/2030', end_date: '18/04/2030',
+                        customer: customer, car_category: car_category)
